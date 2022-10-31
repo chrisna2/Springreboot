d4a0f1bfdb816a9440d3b3371297e86c3c0bd91d
hyunkee <hyunkee@DESKTOP-4HR7UMG>
2022-10-31 14:41:18 +0900

    JPA 마스터링 - 20221031, 나현기

---
+++ src/main/java/com/reboot/repository/PDSBoardRepository.java
@@ -0,0 +1,17 @@
+package com.reboot.repository;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.reboot.domain.PDSBoard;
+
+public interface PDSBoardRepository extends CrudRepository<PDSBoard, Long>{
+	
+	
+	@Modifying //@Query에서는 select만 지원해서 처리한다. 
+	//근데 수정및 입력을 해야되는 경우 해당 @Modifying annotaition을 추가 하면 됨 insert, update, delete 가능
+	@Query("UPDATE FROM PDSFile f SET f.pdsfile = ?2 WHERE f.fno = ?1 ")
+	public int updatePDSFile(Long fno, String newFileName);
+
+}
--- src/test/java/com/reboot/PDSBoardRepositoryTest.java
+++ src/test/java/com/reboot/PDSBoardRepositoryTest.java
@@ -7,17 +7,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.reboot.domain.PDSBoard;
 import com.reboot.domain.PDSFile;
-import com.reboot.repository.PDFBoardRepository;
+import com.reboot.repository.PDSBoardRepository;
 
 @SpringBootTest // 2.5.x 버전 이후 @RunWith은 사라지고 @SpringBootTest에 포함됨
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class PDFBoardRepositoryTest {
+public class PDSBoardRepositoryTest {
 	
 	@Autowired
-	PDFBoardRepository repo;
+	PDSBoardRepository repo;
 	
 	
 	//@Test	//파일 등록
@@ -46,6 +48,27 @@ public class PDFBoardRepositoryTest {
 			Hibernate: update tbl_pds_files set pdsno=? where fno=?
 		*/
 		repo.save(pds);
+	}
+	
+	@Test
+	@Transactional //org.springframework.transaction.annotation.Transactional
+	//TEST 코드에서 해당 annotation을 사용할 경우 해당 트랜잭션의 처리 결과를 롤백처리 한다. 그래서 결과가 commit되지 않는 것.
+	@Commit //org.springframework.test.annotation.Commit; 이걸 추가해 줘야 해당 쿼리의 데이터가 변경된다.
+	public void updatePdsFileTest() {
+		
+		Long fno = 1L;
+		String newName = "updatedFile1.doc";
+		
+		int count = repo.updatePDSFile(fno, newName);
+		
+		System.out.println("update Count : "+count);
+		
+		/* 
+		//이 경우 해당 repository가 PDSBoard 를 대상으로 하기 때문에 PDSBoard 엔티티의 데이터를 조회한다.
+		repo.findById(1L).ifPresent(pds -> {
+			System.out.println(pds);
+		});
+		*/
 	}
 	
 }
